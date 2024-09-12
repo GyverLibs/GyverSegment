8f6940e291d5e546ad54526a01fa3db8b6fbf8fe
AlexGyver <alexgyvershow@ya.ru>
2024-09-12 14:55:15 +0300

    upd

--- src/GyverSegment.h
+++ src/GyverSegment.h
@@ -1,22 +1,22 @@
 #pragma once
 //
-#include "display/DispBare.h"
+#include "core/DispBare.h"
 //
-#include "display/Disp7219.h"
-#include "display/Driver7219.h"
+#include "core/Disp7219.h"
+#include "core/Driver7219.h"
 //
-#include "display/Disp595_4.h"
-#include "display/Disp595_8.h"
-#include "display/Disp595_8v2.h"
-#include "display/Disp595Static.h"
-#include "display/Driver595.h"
+#include "core/Disp595_4.h"
+#include "core/Disp595_8.h"
+#include "core/Disp595_8v2.h"
+#include "core/Disp595Static.h"
+#include "core/Driver595.h"
 //
-#include "display/Disp1637Colon.h"
-#include "display/Disp1637_4.h"
-#include "display/Disp1637_6.h"
-#include "display/Driver1637.h"
+#include "core/Disp1637Colon.h"
+#include "core/Disp1637_4.h"
+#include "core/Disp1637_6.h"
+#include "core/Driver1637.h"
 //
-#include "utils/SegAnimation.h"
-#include "utils/SegAnimationExt.h"
-#include "utils/SegBuffer.h"
-#include "utils/SegRunner.h"
+#include "core/utils/SegAnimation.h"
+#include "core/utils/SegAnimationExt.h"
+#include "core/utils/SegBuffer.h"
+#include "core/utils/SegRunner.h"
